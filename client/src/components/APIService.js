19c44c2de1bdbaffc448d3f31f543fbb9c5cd946
gabriele <vareca@live.com>
2024-05-18 08:57:11 +0900

    delete proxi in package-json

--- client/src/components/APIService.js
+++ client/src/components/APIService.js
@@ -1,7 +1,7 @@
 export default class APIService{
     // Insert an article
     static InsertArticle(body){
-        return fetch(`http://localhost:5000//api/multiply`,{
+        return fetch(`/api/multiply`,{
             'method':'POST',
              headers : {
             'Content-Type':'application/json'
